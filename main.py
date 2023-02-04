efca9e824faedda819e725739310d7237622ce08
jiwoon <jiwoon8425@gmail.com>
2023-02-04 23:34:00 +0900

    11111

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.11111111111
+# Press the green button in the gutter to run the script.2222222
 if __name__ == '__main__':
     print_hi('PyCharm')
 
